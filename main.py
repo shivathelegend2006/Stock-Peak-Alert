98ee6b3090e6ed2b381cc97c0f73f73dd583ad32
shivathelegend2006 <shivathelegend2006@gmail.com>
2026-07-04 02:27:20 +0530

    Testing with real past time series data of NIFTY 50

--- main.py
+++ main.py
@@ -3,17 +3,22 @@
 import time
 from collections import deque #dequeu is higlhy optimised for speed
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from Market import testMarket
 from detector import Velocity
 
-window_size = 300 #5minutiions
+window_size = 375 #5minutiions
 
-market = testMarket()
+df = pd.read_csv("nifty_last_7_days.csv")
+df["date"] = pd.to_datetime(df["date"],dayfirst=True)
+df = df[
+    df["date"].dt.date == pd.Timestamp("2025-07-21").date()
+]
+
 prices = deque(maxlen=window_size)
 velocity_detector = Velocity()
 
-plt.ion()
 
 fig, ax = plt.subplots(figsize=(14,6))
 
@@ -24,26 +29,51 @@ ax.set_xlabel("Ticks")
 ax.set_ylabel("Price")
 ax.grid(True)
 
-n = 0
 c = 0
-while n < 300:
-    price = market.next_pirce()
+vvals = []
+
+alert_x = []
+alert_y = []
+
+for _, row in df.iterrows():
+    price = row["close"]
+
+
     prices.append(price)
 
     velocity = velocity_detector.calc(prices)
+    if velocity is not None:
+        vvals.append(velocity)
+
     line.set_xdata(range(len(prices)))
     line.set_ydata(prices)
+    ax.scatter(alert_x, alert_y, color="red", s=50)
 
     ax.relim()
     ax.autoscale_view()
 
-    plt.draw()
-    plt.pause(0.01)
 
-    if velocity is not None and (velocity < - 0.9 or velocity > 0.9):
-        print(f"Tick: {market.tick:3d} | Price: {price:7.2f} | Velocity: {velocity:6.2f}")
+
+    if velocity is not None and abs(velocity) > 2.9:
+        alert_x.append(len(prices) - 1)
+        alert_y.append(price)
+
+        print(
+            f"{row['date']} | "
+            f"Price: {price:.2f} | "
+            f"Velocity: {velocity:.2f}"
+        )
         c += 1
-    n += 1
-    time.sleep(0.025)
 
 print(c)
+vvals = sorted(vvals)
+
+line.set_xdata(range(len(prices)))
+line.set_ydata(prices)
+
+ax.scatter(alert_x, alert_y, color="red", s=50)
+
+ax.relim()
+ax.autoscale_view()
+
+plt.show()
