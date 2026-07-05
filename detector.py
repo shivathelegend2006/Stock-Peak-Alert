1ac1190d9764c05eb1259b2e5d6c8e73d69eba09
shivathelegend2006 <shivathelegend2006@gmail.com>
2026-07-05 19:19:48 +0530

    Added Acceleration Detector and Testing

--- detector.py
+++ detector.py
@@ -1,8 +1,8 @@
 #Om Namo Venketesaya
 
 class Velocity:
-    def __init__(self,window = 20):
-        self.window = window
+    def __init__(self,lookback = 25):
+        self.window = lookback
 
     def calc(self,prices):
         if len(prices) <= self.window:
@@ -15,3 +15,20 @@ class Velocity:
 
         return velocity
     
+
+class Accelearation:
+    def __init__(self):
+        self.prev_velocity  = None
+
+    def calc(self,velocity):
+        if velocity is None:
+            return None
+        
+        if self.prev_velocity is None:
+            self.prev_velocity = velocity
+            return None
+        
+        accelearation = velocity - self.prev_velocity
+        self.prev_velocity = velocity
+
+        return accelearation
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 from Market import testMarket
-from detector import Velocity
+from detector import Velocity, Accelearation
 
 window_size = 375 #5minutiions
 
@@ -18,6 +18,7 @@ df = df[
 
 prices = deque(maxlen=window_size)
 velocity_detector = Velocity()
+acceleration_detector = Accelearation()
 
 
 fig, ax = plt.subplots(figsize=(14,6))
@@ -30,10 +31,15 @@ ax.set_ylabel("Price")
 ax.grid(True)
 
 c = 0
+n = 0
+
 vvals = []
 
 alert_x = []
 alert_y = []
+
+accel_x = []
+accel_y = []
 
 for _, row in df.iterrows():
     price = row["close"]
@@ -42,19 +48,25 @@ for _, row in df.iterrows():
     prices.append(price)
 
     velocity = velocity_detector.calc(prices)
+    acceleration = acceleration_detector.calc(velocity)
+
     if velocity is not None:
         vvals.append(velocity)
+
 
     line.set_xdata(range(len(prices)))
     line.set_ydata(prices)
     ax.scatter(alert_x, alert_y, color="red", s=50)
-
+    ax.scatter(accel_x, accel_y,
+           color="green",
+           s=50)
+    
     ax.relim()
     ax.autoscale_view()
 
 
 
-    if velocity is not None and abs(velocity) > 2.9:
+    if velocity is not None and (velocity < -2 or velocity > 5):
         alert_x.append(len(prices) - 1)
         alert_y.append(price)
 
@@ -65,7 +77,20 @@ for _, row in df.iterrows():
         )
         c += 1
 
-print(c)
+
+    if acceleration is not None and abs(acceleration) > 1:
+
+        accel_x.append(len(prices)-1)
+        accel_y.append(price)
+
+        print(
+            f"{row['date']} | "
+            f"Acceleration: {acceleration:.2f}"
+        )
+        n += 1
+    
+
+print(c,n )
 vvals = sorted(vvals)
 
 line.set_xdata(range(len(prices)))
